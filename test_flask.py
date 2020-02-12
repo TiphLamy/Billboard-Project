f9b5a4916febb1bee91b58a4f39415dc99813cb0
ANNIE LIM <lian@pc5301-test.esiee.fr>
2020-02-12 17:55:20 +0100

    hist on the page

--- test_flask.py
+++ test_flask.py
@@ -1,13 +1,68 @@
 from pymongo import MongoClient
-from flask import Flask, render_template
+from flask import Flask
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly
 import plotly.graph_objs as go
 import json
 import numpy as np
+from bokeh.embed import json_item
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from jinja2 import Template
+from bokeh.io import show, output_file
+
 from flask import render_template, render_template_string
 app = Flask(__name__)
+
+page = Template("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  {{ resources }}
+</head>
+<body>
+  <div id="myplot"></div>
+  <div id="myplot2"></div>
+  <script>
+  fetch('/plot')
+    .then(function(response) { return response.json(); })
+    .then(function(item) { Bokeh.embed.embed_item(item); })
+  </script>
+  <script>
+  fetch('/plot2')artist = scrap.groupby('artist').size()
+artist = artist.sort_values(ascending=True)
+    .then(function(response) { return response.json(); })
+    .then(function(item) { Bokeh.embed.embed_item(item, "myplot2"); })
+  </script>
+</body>
+""")
+
+
+@app.route('/')
+def root():
+    return page.render(resources=CDN.render())
+
+@app.route('/plot')
+def plot():
+    scrap = pd.read_json("./newscrawler/newscrawler/spiders/scrap200.json")
+    artist = scrap.groupby('artist').size()
+    artist = artist.sort_values(ascending=True)
+
+    #p = figure(title = "Iris Morphology", sizing_mode="fixed", plot_width=400, plot_height=400)
+    #p.xaxis.axis_label = "artist"
+    #p.yaxis.axis_label = "number of albums"
+    #p.circle(artist[-20:].index, artist[-20:].values)
+
+    output_file("bar_sorted.html")
+    p = figure(x_range=list(artist[-20:].index), plot_height=350, title="Album Counts", toolbar_location=None, tools="")
+    p.vbar(x=list(artist[-20:].index), top=list(artist[-20:].values), width=0.9)
+
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+
+    show(p)
+    return redirect_to_url("bar_sorted.html")#json.dumps(json_item(p, "myplot"))
 
 @app.route('/test')
 def index():
