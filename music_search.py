3a290484da3a2177e55afa20f007899ab99f3b60
Tiphanie LAMY VERDIN <lamyvert@pc5301-test.esiee.fr>
2020-02-19 18:28:57 +0100

    Elastic Search 19-02

--- music_search.py
+++ music_search.py
@@ -1,33 +1,74 @@
+# flask env: ./flask_env/bin/activate
+
 from flask import Flask
 from flask import request
 from flask import render_template, render_template_string
 from flask import Flask, flash, redirect, render_template, \
      request, url_for
-
+#from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from search_bar import SearchBar
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+import pandas as pd
+import numpy as np
+
+LOCAL = False
+
+es_client = Elasticsearch(hosts=["localhost" if LOCAL else "elasticsearch"])
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+#app.config['MONGO_URL'] = '27017'
+#mongo = PyMongo(app,uri='mongodb://localhost:27017')
+
 
 @app.route('/')
 def rien():
-    return redirect('/MusicSearch')
+	client = MongoClient()
+	db = client["client_name"]
+	billboard = db["billboard"]
+	return redirect('/MusicSearch')
+
 
 @app.route('/MusicSearch', methods=('GET', 'POST'))
 def MusicSearch():
-    form = MyForm()
+    form = SearchBar()
     if form.validate_on_submit():
-        return redirect('/success/'+form.typing.data)
-    return render_template('sub.html', form=form)
+        return redirect('/information/'+form.typing.data)
+    return render_template('music_search.html', form=form)
 
-@app.route('/success/<search_word>')
+
+@app.route('/information/<search_word>')
 def sucess(search_word):
-    return "Salut " + search_word + ".\nTu es arrivé jusque là. \n <a href='/MusicSearch'> Retour </a>"
+
+	df_billboard = pd.DataFrame(list(billboard.find()))
+	df_billboard_clean = df_billboard.drop(labels='_id',axis='columns')
+	documents = df_billboard_clean.fillna("").to_dict(orient="records")
+
+	for docu in documents:
+		yield {
+			"_index": "albums",
+			"_type": "album",
+			"_source": {k:v if v else None for k,v in docu.items()},
+		}
+
+	QUERY = {
+	  "query": {
+		"multi_match" : {
+		  "query":    search_word,
+		  "fields": [ "artist" ] 
+		}
+	  }
+	}
+
+	result = es_client.search(index="albums", body=QUERY)
+	[elt['_source']['album'] for elt in result["hits"]["hits"]]
+	return render_template('results.html')
 
 
-@app.route('/results', methods=['GET', 'POST'])
-def results():
-  return 'succeed'
 
 
 if __name__ == '__main__':
